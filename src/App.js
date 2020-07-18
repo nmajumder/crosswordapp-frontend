509a10eacb1106c8f7b76fd0e3f6f8f264507749
Nathan Majumder <nathanmajumder@Nathans-MacBook-Pro.local>
2020-07-18 12:20:43 -0400

    google analytics integration

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Fragment } from 'react'
+import ReactGA from 'react-ga'
+import createHistory from 'history/createBrowserHistory'
 import './App.css'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import CrosswordApp from './components/CrosswordApp.jsx'
 import FullCrosswordApp from './components/FullCrosswordApp.jsx'
 import MiniCrosswordApp from './components/MiniCrosswordApp.jsx'
 import StatsApp from './components/StatsApp.jsx'
 import LeaderboardApp from './components/LeaderboardApp.jsx'
-import MobileView from './components/MobileView';
+import MobileView from './components/MobileView'
+
+const history = createHistory()
+ReactGA.initialize('UA-172908124-1');
+history.listen((location, action) => {
+    ReactGA.pageview(location.pathname + location.search);
+    console.log(location.pathname)
+});
 
 function App() {
 
@@ -20,7 +25,7 @@ function App() {
         <MobileView />
       </div>
       <div className="app-wrapper">
-        <Router>
+        <Router history={history}>
           <Switch>
             <Route exact path="/" component={CrosswordApp} />
             <Route path="/crosswords" component={FullCrosswordApp} />
